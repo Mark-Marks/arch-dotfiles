ec9cb91efdad4daa0fef1837fa9aa3a821e6e587
Mark-Marks <iczerwony98@gmail.com>
2022-11-21 18:24:22 +0100

    +setup.sh

---
+++ setup.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+dopackages=true
+doyay=true
+doposh=true
+donerdfonts=true
+dosymlink=true
+
+while getopts ":hpynos" option do
+	case $option in
+		h) echo "-h = view script usage\n-p = don't install packages\n-y = don't install yay\n-p = don't install oh-my-posh\n-n = don't install nerd fonts complete jetbrains\n-s = don't do symlinks"
+			exit;;
+		p) dopackages=false;;
+		y) doyay=false;;
+		n) donerdfonts=false;;
+		o) doposh=false;;
+		s) dosymlink=false;;
+	esac
+done
+
+if [ "$dopackages" = false ] && [ "$doyay" = false ] && [ "$donerdfonts" = false ] && [ "$doposh" = false ] && [ "$dosymlink" = false ]
+then
+	echo "why are you running this script if you don't want it to do anything?"
+	exit 1
+fi
+
+echo "-- !!started setting up!! --"
+
+cd ~
+
+if [ "$dopackages" = true ]
+then
+	echo "started installing packages"
+	# install wanted packages
+	sudo pacman -S --needed zsh wget noto-fonts noto-fonts-emoji git base-devel
+	echo "completed installing packages"
+fi
+
+if [ "$doyay" = true ]
+then
+	echo "started installing yay"
+	# install yay
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+
+	cd ~
+	echo "completed installing yay"
+fi
+
+if [ "$doposh" = true ]
+then
+	echo "started installing oh-my-posh"
+	# install oh-my-posh
+	sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+	sudo chmod +x /usr/local/bin/oh-my-posh
+	echo "completed installing oh-my-posh"
+	
+	echo "started customizing oh-my-posh"
+	# download oh-my-posh themes
+	mkdir ~/.poshthemes
+	wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+	unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+	chmod u+rw ~/.poshthemes/*.omp.*
+	rm ~/.poshthemes/themes.zip
+	echo "completed customizing oh-my-posh"
+fi
+
+# download & install nerdfont complete jetbrains mono
+if [ "$donerdfonts" = true ]
+then
+	echo "started downloading & installing jetbrains mono"
+	mkdir /tmp/fontsInstallation
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip -O /tmp/fontsInstallation/JetBrainsMono.zip
+	unzip /tmp/fontsInstallation/JetBrainsMono.zip
+	mkdir ~/.local/share/fonts/
+	mv /tmp/fontsInstallation/'JetBrains Mono Bold Italic Nerd Font Complete.ttf' ~/.local/share/fonts/
+	mv /tmp/fontsInstallation/'JetBrains Mono Bold Nerd Font Complete.ttf' ~/.local/share/fonts/
+	mv /tmp/fontsInstallation/'JetBrains Mono Italic Nerd Font Complete.ttf' ~/.local/share/fonts/
+	mv /tmp/fontsInstallation/'JetBrains Mono Regular Nerd Font Complete.ttf' ~/.local/share/fonts/
+	rm /tmp/fontsInstallation/*
+	fc-cache
+	echo "completed downloading & installing jetbrains mono"
+fi
+
+# start symlinking, yay!
+if [ "$dosymlink" = true ]
+then
+	echo "started symlinking"
+	ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+	ln -s ~/.dotfiles/.zshrc ~/.zshrc
+	ln -s ~/.dotfiles/.bashrc ~/.bashrc
+	ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+	mkdir ~/.config/macchina
+	mkdir ~/.config/macchina/themes
+	ln -s ~/.dotfiles/macchina.toml ~/.config/macchina/macchina.toml
+	ln -s ~/.dotfiles/hydromez.toml ~/.config/macchina/themes/hydromez.toml
+	echo "completed symlinking"
+fi
+
+echo "-- !!completed setting up!! --"
+exit 0
